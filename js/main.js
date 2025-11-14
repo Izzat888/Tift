a3e54e91836ce8ff8aa475e34446052081f15d8f
Izzat888 <izzatjorayev907@gmail.com>
2025-11-15 00:34:12 +0500

    Tift loyihasi yakunlandi

--- js/main.js
+++ js/main.js
@@ -5,13 +5,35 @@
       nav_conf: "Konferensiyalar",
       nav_home: "Bosh sahifa",
       nav_school: "Ilmiy maktablarimiz",
-      nav_contact: "Bog'lanish"
+      nav_contact: "Bog'lanish",
+      sidebar_news: "Jurnal yangiliklari",
+      sidebar_information: "Jurnal haqida",
+      sidebar_archive: "Arxiv",
+      sidebar_statistics: "Statistika",
+      sidebar_requirements: "Maqola tayyorlash bo‘yicha talablar",
+      sidebar_documents: "Namunaviy hujjatlar",
+      sidebar_copyright: "Mualliflik huquqi",
+      sidebar_staff: "Tahririyat a’zolari",
+      sidebar_journal: "Jurnal",
+      sidebar_AUTHORS: "MUALLIFLARGA",
+      sidebar_EDITORIAL: "TAHRIRIYAT"
     },
     ru: {
       nav_conf: "Конференции",
       nav_home: "Главная",
       nav_school: "Наши научные школы",
-      nav_contact: "Контакты"
+      nav_contact: "Контакты",
+      sidebar_news: "Новости журнала",
+      sidebar_information: "О журнале",
+      sidebar_archive: "Архив",
+      sidebar_statistics: "Статистика",
+      sidebar_requirements: "Требования к подготовке статьи",
+      sidebar_documents: "Образцы документов",
+      sidebar_copyright: "Авторское право",
+      sidebar_staff: "Редакция",
+      sidebar_journal: "Журнал",
+      sidebar_AUTHORS: "Автор",
+      sidebar_EDITORIAL: "РЕДАКЦИЯ"
     }
   };
 
@@ -35,3 +57,12 @@
       btn.addEventListener("click", () => setLanguage(btn.dataset.lang));
     });
   });
+
+    const burger = document.getElementById("burger");
+    const navMenu = document.getElementById("nav-menu");
+    const langSwitch = document.querySelector(".lang-switch");
+
+    burger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+        langSwitch.classList.toggle("active");
+    });
